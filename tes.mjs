3e6e8d2342548fdeafe4fb08f03a9cb3054e2a6a
jalilmboyzt29 <92901357+jalilmboyzt29@users.noreply.github.com>
2025-11-04 02:14:44 +0900

    Create tes.mjs
    Tes

---
+++ tes.mjs
@@ -0,0 +1,7 @@
+// script.mjs
+export function hello() {
+  console.log("Halo dari ES Module!");
+}
+
+// Bisa juga import dari modul lain jika diperlukan
+// import { something } from './another.mjs';
